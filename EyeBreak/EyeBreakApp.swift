1e97d6bdd14d5fd6e99f6e6f4285ee85617f53a8
Shreyash Goli <golishreyash@gmail.com>
2025-11-26 10:46:44 -0800

    Initial Commit

---
+++ EyeBreak/EyeBreakApp.swift
@@ -0,0 +1,32 @@
+//
+//  EyeBreakApp.swift
+//  EyeBreak
+//
+//  Created by Shreyash Goli on 11/26/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct EyeBreakApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ EyeBreakTests/EyeBreakTests.swift
@@ -0,0 +1,17 @@
+//
+//  EyeBreakTests.swift
+//  EyeBreakTests
+//
+//  Created by Shreyash Goli on 11/26/25.
+//
+
+import Testing
+@testable import EyeBreak
+
+struct EyeBreakTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
